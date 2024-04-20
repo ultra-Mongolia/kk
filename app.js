da89f8c1c128068908c68db85aa200cb4fa01536
ultra-Mongolia <167605825+ultra-Mongolia@users.noreply.github.com>
2024-04-20 15:32:11 +0800

    kk

---
+++ app.js
@@ -0,0 +1,40 @@
+console.log(5+10+100-55);
+console.log("hello");
+// + -> toonuudiig hoorond n nemne
+// + -> useg bolon toog hoorond n zalgana
+console.log("Udriin"+" mend")
+console.log("text "+5)
+console.log("5+5="+(5+5))
+// boolean -> true, false
+console.log("boroo oroh uu?"+true);
+// variable zarlah
+var ner = "bayarbold";
+console.log(ner);
+console.log("sainuu. minii neriig "+ ner + " gedeg.");
+console.log("Za sainuu " + ner + " Udriin mend");
+// Number too 
+var too = 25;
+console.log(too+26*10/2);
+var nas = 15;
+var sur = 5 + " shar gedeg ";
+var gender = "unen";
+var on = 2008;
+console.log("minii neriig " + ner + " gedeg." + " bi " + nas + " nastai. " + " bi " + sur + " gazar " + " surdag. " + " bi " + " eregtei " + " gedeg " + " n " + gender + " . ");
+console.log(" ta " + on + " ond " + "tursun ")
+
+var too1 = 35;
+var too2 = 24
+console.log(too1+too2);
+var nasand = 12;
+var odoo = 2024;
+console.log("tanii tursun on : "+(odoo-nasand))
+
+var ondor = 15;
+var urt = 25;
+console.log("p:"+(ondor+urt)*2)
+console.log("s:"+(ondor*urt))
+// y=3*x-7
+// x=5
+// y?
+var x = 5;
+console.log("y="+((3*x)-7))
